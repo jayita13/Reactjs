188add9abd2ab2956a023e7e32a1f03defc2dfd2
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2021-05-31 10:31:42 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ValidComponent from './valid';
-// import FormComponent from './App';
-
-ReactDOM.render(<ValidComponent/>, document.getElementById('root'));
-
-
